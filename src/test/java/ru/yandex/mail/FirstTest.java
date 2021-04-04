92c1a73aeac4cfa0f04db8a520c43bb51ed4e303
Andrei <binofaata@gmail.com>
2021-04-04 21:41:46 +0400

    testing

--- src/test/java/ru/yandex/mail/FirstTest.java
+++ src/test/java/ru/yandex/mail/FirstTest.java
@@ -3,6 +3,9 @@ package ru.yandex.mail;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import javax.swing.*;
 
 
 public class FirstTest extends ChromeDriverSettings {
@@ -15,9 +18,8 @@ public class FirstTest extends ChromeDriverSettings {
         driver.findElement(By.linkText("Войти")).click();
         driver.findElement(By.id("passp-field-login")).sendKeys("binotegari@yandex.ru");
         driver.findElement(By.className("passp-sign-in-button")).click();
-        driver.findElement(By.linkText("ВВедите пароль")).sendKeys("CrimsonKing21");
-//        driver.findElementByXPath("//*[@id=\"passp-field-passwd\"]").sendKeys("CrimsonKing21");
-//        driver.findElement(By.id("passp-field-passwd")).sendKeys("CrimsonKing21");
+        driver.findElement(By.className("Field-label")).click();
+        driver.findElement(By.id("passp-field-passwd")).sendKeys("CrimsonKing21");
 //        driver.findElement(By.className("passp-sign-in-button")).click();
 //        String title = driver.getTitle();
 ////        String button = driver.findElementByLinkText("Войти").getText();
